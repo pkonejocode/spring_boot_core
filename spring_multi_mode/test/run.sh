eeb94202db8d7530a9b5f86d9a63611247fc4d5b
pkonejerocode <pkonejerocode@gmai.com>
2017-02-13 15:05:17 +0100

    Spring multi mode using springBoot

---
+++ spring_multi_mode/test/run.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+cd $(dirname $0)
+
+cd ../complete
+
+./gradlew build
+ret=$?
+if [ $ret -ne 0 ]; then
+exit $ret
+fi
+rm -rf build
+
+cd ../initial
+
+./gradlew compileJava
+ret=$?
+if [ $ret -ne 0 ]; then
+exit $ret
+fi
+rm -rf build
+
+exit
